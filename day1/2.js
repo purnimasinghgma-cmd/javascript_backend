219d9f0a6542163f8ad0d8017adc3cd7ccb5e273
Purnima <purnimasinghgma@gmail.com>
2026-08-12 11:06:33 +0530

    Update 2.js
    change

--- day1/2.js
+++ day1/2.js
@@ -2,14 +2,19 @@ const http = require("http");
 
 const fs = require("fs");
 
-const server = http.createServer(req, res) => {
+const server = http.createServer((req, res) => {
     if (req.url == "/"){
         res.end("Home page");
     } else if (req.url == "/about"){
-        res.end ("about page");
-        
-
+        res.end("about page");
+    } else if (req.url == "/data"){
+        const data = readFileSync("./1jeson", "utf-8");
+        res.end(data);
+    }else{
+        res.end("not found");
     }
-    }
+});
 
-}
+server.listen(8080, () => {
+    console.log("server running at portal 8080");
+})
