b9cecc330d752872c7ed8d5d04a91c77c58f58f0
Purnima <purnimasinghgma@gmail.com>
2026-09-15 22:35:30 +0530

    ch

---
+++ week5/day2/1.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
+app.get("/", (req, res) => {
+    res.send(
+        <form action="/profile" method="post" enctype="multipart/form-data">
+  <input type="file" name="avatar" />
+  <input type ="submit" />
+</form>
+    )
+});
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  if (req.file) {
+    res.send("file uploaded successfully");
+  } else {
+    res.send("something went wrong");
+  }
+  });
+
+  app.listen(8080, () => {
+    console.log("serever started")
+  });
+
